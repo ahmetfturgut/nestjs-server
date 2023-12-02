af8fdaaea234692fc100a302831db621363429c2
ahmetfturgut <aft@bugbounter.com>
2023-12-02 17:27:24 +0300

    added class validation

--- src/app/user/dto/create-user.dto.ts
+++ src/app/user/dto/create-user.dto.ts
@@ -1,17 +1,48 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Max, Min } from "class-validator";
+
 export class CreateUserRequestDto {
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsString()
+    @IsNotEmpty()
     surname: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Min(6)
+    @Max(10)
     password: string;
 
 }
 
 
 export class UpdateUserRequestDto {
+
+    @IsString()
+    @IsNotEmpty()
     id: string
+
+    @IsString()
+    @IsOptional()
     email: string;
+
+    @IsString()
+    @IsOptional()
     name: string;
+
+    @IsString()
+    @IsOptional()
     surname: string;
+
+    @IsString()
+    @IsOptional()
     password: string;
 
 }
